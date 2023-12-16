d7e417e16eb2071fea4a467acb8bed9e919fd73a
rodrigue.midbel <rodrigue.midbel@gmail.com>
2023-12-16 13:01:35 +0000

    Nouvelle fonction personnalisée

--- bonjour.py
+++ bonjour.py
@@ -1,2 +1,4 @@
-def saluer():
-    return "Bonjour"
+#------Dernier jour de formation fullstack
+#------Date : 16-12-23
+def saluer_der():
+    return "Bonjour M. Der ! \n j'espere que le sejour à Niamey se passe bien"
